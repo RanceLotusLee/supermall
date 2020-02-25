27fc9b970231f483c93b15ff49d00cc9fce14621
Rance <13626112170@163.com>
2020-02-26 03:23:21 +0800

    封装了NavBar组件

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot></slot>
+    </div>
+    <div class="center">
+      <slot></slot>
+    </div>
+    <div class="right">
+      <slot></slot>
+    </div>
+ 
+  </div>
+</template>
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+}
+
+.left ,.right{
+  width: 46px;
+  background-color: aqua;
+}
+
+.center {
+  flex: 1;
+  text-align: center;
+  background-color: red;
+}
+</style>
